9d089458ac3eddde0a7b6550dfcd9e6ae3f614a4
Masataka Aizawa <aizawa@utap.phys.s.u-tokyo.ac.jp>
2022-04-26 19:13:05 +0800

    change modules

--- tests/check_lc_tpfs.py
+++ tests/check_lc_tpfs.py
@@ -9,10 +9,10 @@ import astropy.units as u
 import os
 
 
-def main(target_for_dl, exptime=120, out_dir ="./data", filter_length = 4501):
+def main(target_for_dl, exptime=120, out_dir ="./data",  filter_length =7201):
+
     target = target_for_dl.replace(" ","")
     out_dir_for_target = utils.make_output_folder_for_target(target, out_dir)
-    radius_gaia_search = (12/2.0) * 20*u.arcsec*1.5
 
     ## lc analysis
     print("lc analysis")
@@ -26,10 +26,7 @@ def main(target_for_dl, exptime=120, out_dir ="./data", filter_length = 4501):
             return None
         lc_ana.save_lcs(lc_collection, search_result, out_dir_for_target)
         lc_ana.save_search_result(search_result, target, out_dir_for_target)
-    print(search_result)
-    file_heads = utils.make_output_name_for_lcs(search_result)
-    plotter_lc.plot_lcs(lc_collection, target, file_heads, out_dir_for_target, filter_length, exptime)
-    periods = lc_ana.get_periods(lc_collection)
+    periods = lc_ana.get_periods(lc_collection, filter_length )
 
 
     ## tpf analysis
@@ -46,40 +43,23 @@ def main(target_for_dl, exptime=120, out_dir ="./data", filter_length = 4501):
             return None        
         ana_tpf.save_tpf(tpfs, search_result_tpf,  out_dir_for_target)
         ana_tpf.save_search_result_tpf(search_result_tpf, target, out_dir_for_target)
-    print(search_result_tpf)
 
-    file_heads_tpf = utils.make_output_name_for_tpfs(search_result_tpf)
-    ra, dec, gaia_mag, gaia_stars = utils.get_gaia_stars(target, radius_gaia_search)
-    plotter_tpf.plot_tpfs_with_gaia(tpfs, target, out_dir_for_target, gaia_stars, file_heads_tpf)
+    files = plotter_tpf_all.make_output_name_for_tpfs_lc(search_result_tpf, out_dir_for_target )
+    plotter_tpf_all.lcs_for_tpfs(tpfs, files,  filter_length )
 
-
-    ## differentail imaging
-    print("differential imaging")
-    avg_images, diff_images, periods, folded_lcs, folded_bin_lcs = ana_tpf.make_difference_image_for_tpfs(tpfs, \
-        period_inputs  = periods, aperture_mask ="default", filter_length = filter_length)
-    wcs_arr = wcs_tpf.get_wcs_from_tpfs(tpfs)
-    xy_cen_arr, ra_dec_cen_arr = ana_tpf.compute_centroids_for_images(diff_images, wcs_arr, ra[0], dec[0])
-
-    plotter_tpf.plot_diffimages_with_gaia(avg_images, diff_images, xy_cen_arr, target, \
-        gaia_stars, periods,file_heads_tpf,  wcs_arr, out_dir=out_dir_for_target)
-
-    files = plotter_tpf_all.make_output_name_for_tpfs_periodogram(search_result, out_dir_for_target)
-    plotter_tpf_all.periodogram_for_tpfs(tpfs, files,periods, filter_length)
-
-    files = plotter_tpf_all.make_output_name_for_tpfs_lc(search_result, out_dir_for_target)
-    plotter_tpf_all.lcs_for_tpfs(tpfs, files, filter_length)
-    utils.save_results(gaia_stars, periods, ra_dec_cen_arr, out_dir_for_target)
-
+    files = plotter_tpf_all.make_output_name_for_tpfs_periodogram(search_result_tpf, out_dir_for_target)
+    plotter_tpf_all.periodogram_for_tpfs(tpfs, files, periods, filter_length)
 
 if __name__ == "__main__":
+
     targets_for_dl=  ["TIC 121462259", "TIC 130414928", "TIC 141522677", "TIC 144069454"]
     exptime=120 # seconds
     out_dir ="./data"
-    filter_length_day = 5 #day
+    filter_length_day = 5.0 #day
     filter_length = utils.convert_filter_width(filter_length_day, exptime)
+    print("filter_wd:", filter_length)
 
     if filter_length %2==0:
         print("filter_length must be odd number!!")
     else:
-        for target in targets_for_dl:
-            main(target, exptime, out_dir, filter_length )
+        main(target_for_dl, exptime, out_dir, filter_length )
